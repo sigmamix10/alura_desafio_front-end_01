9ea8d9487e4cde01839f838caaa535b9972516c9
sigmamix10 <94759949+sigmamix10@users.noreply.github.com>
2022-03-25 11:09:25 -0300

    Experiencias e Formulario
    Acressimo das seções de experiencias e o formulario de contato

---
+++ js/validador.js
@@ -0,0 +1,10 @@
+function validarDados(){
+   var nome = document.getElementById('nome')
+   var email = document.getElementById('email')
+   var assunto =  document.getElementById('assunto')
+   var mensagem = document.getElementById('mensagem')
+
+   if(nome == ''){
+      alert('Campo vazio')
+   }
+}
